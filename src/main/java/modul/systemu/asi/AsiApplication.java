64840670efbb49b330fc9bcf1ae4d71db40590ff
Marcin <swistak8452@gmail.com>
2020-01-02 17:49:14 +0100

    Inicjalizacja projektu

---
+++ src/main/java/modul/systemu/asi/AsiApplication.java
@@ -0,0 +1,13 @@
+package modul.systemu.asi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/modul/systemu/asi/AsiApplicationTests.java
@@ -0,0 +1,13 @@
+package modul.systemu.asi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
